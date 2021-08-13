baa11139f474524f834a0c852c972cf8c34b1538
Ivan <1095036273@qq.com>
2021-08-13 18:26:35 +0800

    feat: 添加ts检查

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <img alt="Vue logo" src="./assets/logo.png" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const a: number = '1'
+console.log(a)
+</script>
 
 <style lang="scss">
 #app {
