f2de6d7cdaa5371340638d7d56e6fe892facfe9f
Ivan <1095036273@qq.com>
2021-08-16 11:11:04 +0800

    feat: 封装引入vant

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp, defineComponent } from 'vue'
 import App from './App.vue'
-import 'vant/es/button/style'
 import installVant from './vant'
 const app = createApp(App)
 
--- src/vant.ts
+++ src/vant.ts
@@ -1,5 +1,6 @@
 import Button from 'vant/es/button'
 import { App } from '@vue/runtime-core'
+import 'vant/es/button/style'
 const installVant = (app: App) => {
   app.component(Button.name, Button)
   return app
